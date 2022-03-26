c410a8d0d282f453cf6308986d9678768588b7ea
shimada337 <eisukeqoo@icloud.com>
2022-03-26 09:54:14 +0000

    [add]activestorage

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,4 +7,11 @@ class UsersController < ApplicationController
 
   def edit
   end
+  
+  private
+  
+  def user_params
+    params.require(:user).permit(:name, :introduction, :image)
+  end
+  
 end
