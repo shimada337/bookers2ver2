a6d517d798e37bd93a05cc4b273676e8832dcd93
shimada337 <eisukeqoo@icloud.com>
2022-03-27 05:13:08 +0000

    [add]user,update機能

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,6 +14,12 @@ class UsersController < ApplicationController
   def edit
     @user = User.find(params[:id])
   end
+  
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(@user.id)
+  end
 
   private
 
