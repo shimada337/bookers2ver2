9cbecc55166717d3ffe0522c55f111d82ede1632
shimada337 <eisukeqoo@icloud.com>
2022-04-03 09:26:50 +0000

    ゲストログイン機能

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :ensure_guest_user, only: [:edit]
   def index
     @user = current_user
     @users = User.all
@@ -35,4 +36,10 @@ class UsersController < ApplicationController
     params.require(:user).permit(:name, :introduction, :image)
   end
 
+  def ensure_guest_user
+    @user = User.find(params[:id])
+    if @user.name == "guestuser"
+      redirect_to user_path(current_user), notice: 'ゲストユーザーはプロフィール編集画面へ遷移できません。'
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
   get "home/about" => "homes#about"
   resources :books, only:[:index, :show, :edit, :create, :destroy, :update]
   resources :users, only:[:index, :show, :edit, :update]
+  devise_scope :user do
+    post 'users/guest_sign_in',to:'users/sessions#guest_sign_in'
+  end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
