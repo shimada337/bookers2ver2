e98638033e668ba68bc726ca4a5dc06e766450af
shimada337 <eisukeqoo@icloud.com>
2022-04-04 01:32:20 +0000

    Rubocop実行

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,24 +4,24 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-         has_many :books, dependent: :destroy
+  has_many :books, dependent: :destroy
 
-         has_one_attached :profile_image
+  has_one_attached :profile_image
 
-         validates :name, uniqueness: true,
-           length: { minimum: 2, maximum: 20 }
-         validates :introduction, length: { maximum: 50 }
+  validates :name, uniqueness: true,
+                   length: { minimum: 2, maximum: 20 }
+  validates :introduction, length: { maximum: 50 }
 
-  def get_profile_image(width,height)
+  def get_profile_image(width, height)
     unless profile_image.attached?
       file_path = Rails.root.join('app/assets/images/no_image.jpeg')
       profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
     end
-    profile_image.variant(resize_to_limit: [width,height]).processed
+    profile_image.variant(resize_to_limit: [width, height]).processed
   end
-  
+
   def self.guest
-    find_or_create_by!(name:'guestuser',email:'guest@example.com') do |user|
+    find_or_create_by!(name: 'guestuser', email: 'guest@example.com') do |user|
       user.password = SecureRandom.urlsafe_base64
       user.name = "guestuser"
     end
