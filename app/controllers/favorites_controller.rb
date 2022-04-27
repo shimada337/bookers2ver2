010b02c3be3d26a89ea86ae1ff0e60bf18af8dcc
shimada337 <eisukeqoo@icloud.com>
2022-04-27 08:07:32 +0000

    [update]favoriteコントローラ

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,7 +1,13 @@
 class FavoritesController < ApplicationController
   def create
+    @book_favorite = Favorite.new(user_id: current_user.id, book_id: params[:book_id])
+    @book_favorite.save
+    redirect_to books_path
   end
 
   def destroy
+    @book_favorite = Favorite.find_by(user_id: current_user.id, book_id: params[:book_id])
+    @book_favorite.destroy
+    redirect_to books_path
   end
 end
