ef632da5ab6cdae482f605591cef1c07ec1f398c
shimada337 <eisukeqoo@icloud.com>
2022-04-27 09:18:26 +0000

    [add]いいね機能

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,6 +1,10 @@
 class Book < ApplicationRecord
   belongs_to :user
   has_many :favorites, dependent: :destroy
+  
+  def favorited_by?(user)
+    favorites.exists?(user_id: user.id)
+  end
 
   validates :title, presence: true
   validates :body, presence: true, length: { maximum: 200 }
