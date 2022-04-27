7d840c4f3f54b9cdd5aff71a3bdb5a72f852eae2
shimada337 <eisukeqoo@icloud.com>
2022-04-27 08:03:57 +0000

    [add]favoriteコントローラ

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "homes#top"
   get "home/about" => "homes#about"
-  resources :books, only: [:index, :show, :edit, :create, :destroy, :update]
+  resources :books, only: [:index, :show, :edit, :create, :destroy, :update] do
+    resource :favorites, only: [:create, :destroy]
+  end
   resources :users, only: [:index, :show, :edit, :update]
   devise_scope :user do
     post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
