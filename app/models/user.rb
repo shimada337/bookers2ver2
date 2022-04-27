44cce41642b24547245ad66b29b7a487be192fd4
shimada337 <eisukeqoo@icloud.com>
2022-04-27 07:49:18 +0000

    [fix]favoriteアソシエーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :books, dependent: :destroy
-  has_many :fa
+  has_many :favorites
 
   has_one_attached :profile_image
 
